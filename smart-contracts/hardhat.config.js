9ad7708c0c87ab2bd9a7e2ab0200c7e0c0298154
Shouvik <01.shouvik@gmail.com>
2023-06-16 14:07:47 +0200

    initial update to remove blockers

--- smart-contracts/hardhat.config.js
+++ smart-contracts/hardhat.config.js
@@ -5,7 +5,13 @@ module.exports = {
     goerli: {
       url: 'https://eth-goerli.g.alchemy.com/v2/EUL_b0M4xTEMnUpJvDAZpSdRJQiKNWid',
       accounts: [
-        '',
+        '7e7460a37206177f34de4206c63018658500c27d9925c3e97c147ec4147f23c2',
+      ],
+    },
+    sepolia: {
+      url: 'https://eth-sepolia.g.alchemy.com/v2/UNdIZtG0dqvQktAMq3ABxntm1eAu-mXL',
+      accounts: [
+        '899bce550bd4c981b5ca2a9bff04bc68ef4a5bf83a9448bca7056e2b0429e3ad',
       ],
     },
   },
