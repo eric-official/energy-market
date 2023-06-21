d7bc22dd1e8dfb9a1f1d570de7892478490a8010
Dominik Mehringer <Dominik.Mehringer@live.de>
2023-06-21 21:56:16 +0200

    Oracle set

--- smart-contracts/contracts/oracle/TradingContextOracle.sol
+++ smart-contracts/contracts/oracle/TradingContextOracle.sol
@@ -4,54 +4,61 @@ pragma solidity >=0.7.0 <0.9.0;
 import "@openzeppelin/contracts/access/AccessControl.sol";
 import "./ICaller.sol";
 
-contract RandOracle is AccessControl {
+contract TradingContextOracle is AccessControl {
     bytes32 public constant PROVIDER_ROLE = keccak256("PROVIDER_ROLE");
 
     uint private numProviders = 0;
     uint private providersThreshold = 1;
-    uint private randNonce = 0;
 
     mapping(uint256=>bool) private pendingRequests;
 
-    struct Response {
+    struct ResponseCo2Price {
         address providerAddress;
         address callerAddress;
-        uint256 randomNumber;
+        uint256 price;
     }
 
-    mapping(uint256=>Response[]) private idToResponses;
+    struct ResponseCo2PerKwh {
+        address providerAddress;
+        address callerAddress;
+        uint256 co2PerKwh;
+    }
+
+    struct ResponseSpotPrice {
+        address providerAddress;
+        address callerAddress;
+        uint256 co2PerKwh;
+    }
+
+
+    mapping(uint256=>ResponseCo2Price[]) private idToResponsesPrice;
+    mapping(uint256=>ResponseCo2PerKwh[]) private idToResponsesCo2PerKwh;
+    mapping(uint256=>ResponseSpotPrice[]) private idToResponsesSpotPrice;
 
     constructor() {
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender); // make the deployer admin
     }
 
-    function returnRandomNumber(uint256 randomNumber, address callerAddress, uint256 id) external onlyRole(PROVIDER_ROLE) {
+    function setCo2PricePerTon(uint256 price, address callerAddress, uint256 id) public {
         require(pendingRequests[id], "Request not found.");
 
         // Add newest response to list
-        Response memory res = Response(msg.sender, callerAddress, randomNumber);
-        idToResponses[id].push(res);
-        uint numResponses = idToResponses[id].length;
+        ResponseCo2Price memory res = ResponseCo2Price(msg.sender, callerAddress, price);
+        idToResponsesPrice[id].push(res);
+        uint numResponses = idToResponsesPrice[id].length;
         // Check if we've received enough responses
         if (numResponses == providersThreshold) {
-            uint compositeRandomNumber = 0;
-
-            // Loop through the array and combine responses
-            for (uint i=0; i < idToResponses[id].length; i++) {
-                compositeRandomNumber = compositeRandomNumber ^ idToResponses[id][i].randomNumber; // bitwise XOR
-            }
-
+           
             // Clean up
             delete pendingRequests[id];
-            delete idToResponses[id];
+            delete idToResponsesPrice[id];
 
             // Fulfill request
-            ICaller(callerAddress).fulfillRandomNumberRequest(compositeRandomNumber, id);
+            ICaller(callerAddress).fulfillCo2PricePerTonRequest(price, id);
 
-            emit RandomNumberReturned(compositeRandomNumber, callerAddress, id);
+            emit Co2PricePerTonReturned(price, callerAddress, id);
         }
     }
-
 
     function requestCo2PricePerTon() external returns (uint256) {
         require(numProviders > 0, " No data providers not yet added.");
@@ -64,6 +71,28 @@ contract RandOracle is AccessControl {
         return id;
     }
 
+    function setGramCo2PerKwh(uint256 gram, address callerAddress, uint256 id) public {
+        require(pendingRequests[id], "Request not found.");
+
+        // Add newest response to list
+        ResponseCo2PerKwh memory res = ResponseCo2PerKwh(msg.sender, callerAddress, gram);
+        idToResponsesCo2PerKwh[id].push(res);
+        uint numResponses = idToResponsesCo2PerKwh[id].length;
+        // Check if we've received enough responses
+        if (numResponses == providersThreshold) {
+           
+            // Clean up
+            delete pendingRequests[id];
+            delete idToResponsesCo2PerKwh[id];
+
+            // Fulfill request
+            ICaller(callerAddress).fulfillGramCo2PerKwhRequest(gram, id);
+
+            emit Co2PerKwhReturned(gram, callerAddress, id);
+        }
+        
+    }
+
     function requestGramCo2PerKwh() external returns (uint256) {
         require(numProviders > 0, " No data providers not yet added.");
 
@@ -73,6 +102,27 @@ contract RandOracle is AccessControl {
 
         emit Co2PerKwhRequested(msg.sender, id);
         return id;
+    }
+
+    function setSpotPrice(uint256 spotPrice, address callerAddress, uint256 id) public {
+         require(pendingRequests[id], "Request not found.");
+
+        // Add newest response to spotPrice
+        ResponseSpotPrice memory res = ResponseSpotPrice(msg.sender, callerAddress, spotPrice);
+        idToResponsesSpotPrice[id].push(res);
+        uint numResponses = idToResponsesSpotPrice[id].length;
+        // Check if we've received enough responses
+        if (numResponses == providersThreshold) {
+           
+            // Clean up
+            delete pendingRequests[id];
+            delete idToResponsesSpotPrice[id];
+
+            // Fulfill request
+            ICaller(callerAddress).fulfillSpotPriceRequest(spotPrice, id);
+
+            emit Co2PerKwhReturned(spotPrice, callerAddress, id);
+        } 
     }
 
     function requestSpotPrice() external returns (uint256) {
@@ -119,8 +169,7 @@ contract RandOracle is AccessControl {
     
     event Co2PerKwhRequested(address callerAddress, uint id);
     event Co2PerKwhReturned(uint256 co2PricePerKwh, address callerAddress, uint id);
-    
-    
+        
     event SpotPriceRequested(address callerAddress, uint id);
     event SpotPriceReturned(uint256 spotPrice, address callerAddress, uint id);
 
