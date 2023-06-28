66f811268995e85be84103490d913e7bc8aecd7c
EricOfficial <eric.naeser@gmx.de>
2023-06-28 15:36:51 +0200

    small Ui changes

--- client/src/views/admin/default/components/MonthlyTrafficCard.jsx
+++ client/src/views/admin/default/components/MonthlyTrafficCard.jsx
@@ -96,21 +96,13 @@ const MonthlyTrafficCard = ({connectedAccount}) => {
     }, [connectedAccount])
     return (
         <Card extra="!p-[20px] text-center">
-
+            <div>
+                <h4 className="text-lg font-bold text-navy-700 dark:text-white">
+                    Your Energy Capacity
+                </h4>
+            </div>
             <div
                 className="flex h-full w-full flex-row justify-between sm:flex-wrap lg:flex-nowrap 2xl:overflow-hidden">
-                <div className="flex flex-col">
-                    <p className="mt-[20px] text-3xl font-bold text-navy-700 dark:text-white">
-                        $37.5K
-                    </p>
-                    <div className="flex flex-col items-start">
-                        <p className="mt-2 text-sm text-gray-600">Total Spent</p>
-                        <div className="flex flex-row items-center justify-center">
-                            <MdArrowDropUp className="font-medium text-green-500"/>
-                            <p className="text-sm font-bold text-green-500"> +2.45% </p>
-                        </div>
-                    </div>
-                </div>
                 <div className="h-full w-full">
                     {!connectedAccount ? (
                         <p className="mt-[20px] text-3xl font-bold text-navy-700 dark:text-white">
